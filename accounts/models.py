cb0bdf3e67f62c4578526d767277d3586d9bf7cd
rileyracer62@yahoo.com <rileyracer62@yahoo.com>
2022-07-19 19:37:34 -0700

    created base custom user, added forms. py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=75, null=True)
+    age = models.PositiveIntegerField(null=True, blank=True)

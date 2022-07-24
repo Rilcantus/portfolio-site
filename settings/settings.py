dd1f38313e8d5f077f49e6f664fad6959118f15d
rileyracer62@yahoo.com <rileyracer62@yahoo.com>
2022-07-23 22:20:27 -0700

    updated the password_resets.htmls

--- settings/settings.py
+++ settings/settings.py
@@ -135,4 +135,4 @@ LOGOUT_REDIRECT_URL = "home"
 CRISPY_ALLOWED_TEMPLATE_PACK = "bootstrap5"
 CRISPY_TEMPLATE_PACL = "bootstrap5"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBacke"

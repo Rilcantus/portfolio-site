336dd779be84dbdbbab7db50a5eec91f4810419c
rileyracer62@yahoo.com <rileyracer62@yahoo.com>
2022-07-24 13:44:38 -0700

    added project views, detail, udpate delete

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
 
-from .views import ProjectListView
+from .views import (
+    ProjectListView,
+    ProjectDetailView,
+    ProjectUpdateView,
+    ProjectDeleteView,
+
+)
 
 urlpatterns = [
+    path("<int:pk>/", ProjectDetailView.as_view(),
+        name="project_detail"),
+    path("<int:pk/edit/", ProjectUpdateView.as_view(),
+        name="project_edit"),
+    path("<int:pk>/delete/", ProjectDeleteView.as_view(),
+        name="project_delete"),
     path("", ProjectListView.as_view(), name="project_list"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Project
 
@@ -7,3 +9,22 @@ class ProjectListView(ListView):
     model = Project
     template_name: str = "project_list.html"
 
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name: str = "project_detail.html"
+
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    fields = (
+        "title",
+        "overview",
+    )
+    template_name: str = "project_edit.html"
+
+
+class ProjectDeleteView(DeleteView):
+    model = Project
+    template_name: str = "project_delete.html"
+    success_url = reverse_lazy("project_list")

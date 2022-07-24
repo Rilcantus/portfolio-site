14c3009587d1887e1ad1236eb42db83fe2264250
rileyracer62@yahoo.com <rileyracer62@yahoo.com>
2022-07-24 13:20:48 -0700

    created project_list html view worked after block issue

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Project
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name: str = "project_list.html"
+
